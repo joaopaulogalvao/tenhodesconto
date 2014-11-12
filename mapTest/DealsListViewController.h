1a854abdd0602bbcecbd95612ab0c2889962f2b1
jalves59 <jalves59@ford.com>
2014-11-12 09:29:29 -0200

    added DetailsListViewController class subclassing it to PFQueryTableViewController

---
+++ mapTest/DealsListViewController.h
@@ -0,0 +1,13 @@
+//
+//  DealsListViewController.h
+//  mapTest
+//
+//  Created by Joao Alves on 11/12/14.
+//  Copyright (c) 2014 joaopaulo. All rights reserved.
+//
+
+#import <Parse/Parse.h>
+
+@interface DealsListViewController : PFQueryTableViewController
+
+@end
