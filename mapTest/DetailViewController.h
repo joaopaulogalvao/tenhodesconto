154c59eff5ab0764bbd67fe431fe6f5d56e93e22
jalves59 <jalves59@ford.com>
2014-11-19 08:34:54 -0200

    Added DetailViewController and its view and configuration on storyboard

---
+++ mapTest/DetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.h
+//  mapTest
+//
+//  Created by Joao Alves on 11/19/14.
+//  Copyright (c) 2014 joaopaulo. All rights reserved.
+//
+
+#import <Parse/Parse.h>
+
+@interface DetailViewController : PFQueryTableViewController
+
+@end
